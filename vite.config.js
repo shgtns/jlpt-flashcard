dce85de464de276ee72cd5faf6743f295db472f8
Angelo <angelo.negri@unimi.it>
2025-11-12 17:07:28 +0100

    corretto base

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    base: '/jlpt-flashcard/', // 👈 usa il nome ESATTO del tuo repository
 })
